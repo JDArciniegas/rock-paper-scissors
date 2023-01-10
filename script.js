f25bf98f42d3a351d20ee8d7b60fe642bb06c18b
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-09 23:44:39 -0700

    created and tested getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// Rock paper scissors
+// create function to randomly select rock, paper, scissors
+// create function to pay a round computer vs player
+
+function getComputerChoice(){
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
+}
+
+console.log(getComputerChoice());
